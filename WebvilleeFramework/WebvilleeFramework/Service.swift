d1203a068c6792f46254276418840102e031cf51
sanjay singh jadon <sanjay.webvillee@gmail.com>
2020-09-08 23:09:32 +0530

    Finisd Framework

---
+++ WebvilleeFramework/WebvilleeFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  WebvilleeFramework
+//
+//  Created by WV-Mac4 on 08/09/20.
+//  Copyright © 2020 Mac-4. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func getString() -> String {
+        return "Generated string by WebvilleeFramework"
+    }
+}
---
+++ WebvilleeFramework/WebvilleeFramework/WebvilleeFramework.h
@@ -0,0 +1,19 @@
+//
+//  WebvilleeFramework.h
+//  WebvilleeFramework
+//
+//  Created by WV-Mac4 on 08/09/20.
+//  Copyright © 2020 Mac-4. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebvilleeFramework.
+FOUNDATION_EXPORT double WebvilleeFrameworkVersionNumber;
+
+//! Project version string for WebvilleeFramework.
+FOUNDATION_EXPORT const unsigned char WebvilleeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebvilleeFramework/PublicHeader.h>
+
+
